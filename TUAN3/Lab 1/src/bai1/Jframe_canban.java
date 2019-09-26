b6622c32bfa0657c66dab307c42ba2f0067aaa2f
khanguyen1000 <55082300+khanguyen1000@users.noreply.github.com>
2019-09-26 17:31:54 +0700

    lab 1

---
+++ TUAN3/Lab 1/src/bai1/Jframe_canban.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai1;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author KHA
+ */
+public class Jframe_canban extends JFrame{
+   public Jframe_canban(){
+       setVisible(true);
+       setTitle("Lab 1");
+       setBackground(Color.gray);
+       setSize(400,400);
+       
+     }
+         
+           public static void main(String[] args) {
+               new Jframe_canban();
+              
+           }
+           
+       };
